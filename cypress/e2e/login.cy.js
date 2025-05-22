bb0d88837b0323cd511cc735db7861d1c10e102b
Gerfson Alves <gerfson17@hotmail.com>
2025-05-22 16:03:12 -0300

    Login com email e senha correta

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,22 +3,36 @@ import { faker } from '@faker-js/faker';
 
 describe('Testes de login', () => {
 
+  // Variáveis para usar em outros 'it' os mesmos dados
+  let nomeCompleto, email, senha;
+
+  before(() => {
+    nomeCompleto = faker.person.fullName();
+    email = faker.internet.email();
+    senha = faker.internet.password();
+  })
+
   beforeEach(() => {
     cy.visit('https://automationexercise.com/')
-
   })
 
   it('Registrar um novo usuário', () => {
+
+    // Iniciando com a verificação da página correta para o registro
     cy.get('a[href="/login"]')
       .should('be.visible')
       .click()
     cy.url().should('include', '/login')
-    cy.get('[data-qa="signup-name"]').type(faker.person.fullName())
-    cy.get('[data-qa="signup-email"]').type(faker.internet.email())
+
+    // Inicio do processo para registro do usuário
+    cy.get('[data-qa="signup-name"]').type(nomeCompleto)
+    cy.get('[data-qa="signup-email"]').type(email)
     cy.get('[data-qa="signup-button"]').click()
+
+    // Preenchendo e selecionando todos os dados do novo usuário
     cy.url().should('include', '/signup')
     cy.get('#id_gender1').click()
-    cy.get('[data-qa="password"]').type(faker.internet.password())
+    cy.get('[data-qa="password"]').type(senha)
     cy.get('[data-qa="days"]').select('5')
     cy.get('[data-qa="months"]').select('June')
     cy.get('[data-qa="years"]').select('1993')
@@ -31,8 +45,24 @@ describe('Testes de login', () => {
     cy.get('[data-qa="city"]').type(faker.location.city())
     cy.get('[data-qa="zipcode"]').type(faker.location.zipCode())
     cy.get('[data-qa="mobile_number"]').type(faker.phone.number())
+
+    // Clicando no botão e finalizando o registro do usuário
     cy.get('[data-qa="create-account"]').click()
+
+    // Verificando se a conta foi criada com sucesso
     cy.contains('Account Created!').should('be.visible')
   })
+
+  it('Login com email e senha correta', () => {
+    cy.get('a[href="/login"]')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', '/login')
+
+    cy.get('[data-qa="login-email"]').type(email)
+    cy.get('[data-qa="login-password"]').type(senha)
+    cy.get('[data-qa="login-button"]').click()
+
+  });
 
 })
